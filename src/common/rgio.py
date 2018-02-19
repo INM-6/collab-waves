fc468a2432ec8068a04f7bba5ec7746229e15ab7
Michael Denker <m.denker@fz-juelich.de>
2018-02-20 00:12:39 +0100

    Attempted bug fix at weird Python3 issue where a class method is not found

--- src/common/rgio.py
+++ src/common/rgio.py
@@ -226,13 +226,16 @@ class ReachGraspIO(BlackrockIO):
 
     __grip_first_str = [''.join(i) for i in list(itertools.product(
         list(__grip.keys()), list(__force.keys())))]
+    a=list(__grip.keys())
     __grip_only_str = [[''.join(i) for i in list(itertools.product(
-        [list(__grip.keys()[j])], [list(__grip.keys()[j])]))][0] for j in range(2)]
+        [a[j]], [a[j]]))][0] for j in range(2)]
+    __grip_only_str = [[''.join(i) for i in list(itertools.product(
+        [list(__grip.keys())[j]], [list(__grip.keys())[j]]))][0] for j in range(2)]
 
     __force_first_str = [''.join(i) for i in list(itertools.product(
         list(__force.keys()), list(__grip.keys())))]
     __force_only_str = [[''.join(i) for i in list(itertools.product(
-        [list(__force.keys()[j])], [list(__force.keys()[j])]))][0] for j in range(2)]
+        [list(__force.keys())[j]], [list(__force.keys())[j]]))][0] for j in range(2)]
 
     __list_trial_types = __grip_first_str + __grip_only_str + \
         __force_first_str + __force_only_str
